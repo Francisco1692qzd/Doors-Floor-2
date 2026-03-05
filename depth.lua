b0b9bb4b1bb4fab45db597087a377a8ddf01cb31
Francisco1692qzd <franciscomine16432@gmail.com>
2026-03-05 20:43:57 -0300

    Update depth.lua

--- depth.lua
+++ depth.lua
@@ -165,7 +165,7 @@ task.spawn(function()
 					end
 
 					remotesFolder.Parent.GameStats["Player_".. v.Character.Name].Total.DeathCause.Value = "Depth"
-					firesignal(remotesFolder.DeathHint.OnClientEvent, {"I'd never expect to have a rare entity...", "You can feel him nearby whenever the lights flicker...", "Hide or search for a safe spot!"})
+					firesignal(remotesFolder.DeathHint.OnClientEvent, {"I'd never expect to have a rare entity...", "You can feel him nearby whenever the lights flicker...", "Hide or search for a safe spot!"}, "Blue")
 
 					task.wait(0.7)
 					gui:Destroy()

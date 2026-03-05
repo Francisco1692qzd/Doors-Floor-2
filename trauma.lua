61bfa19fe4d18dba99f9530ce1e5946f627d9d4f
Francisco1692qzd <franciscomine16432@gmail.com>
2026-03-05 15:15:40 -0300

    Update trauma.lua

--- trauma.lua
+++ trauma.lua
@@ -65,7 +65,7 @@ task.spawn(function()
 	end
 	local currentRoom = workspace.CurrentRooms:FindFirstChild(roomValues.Value)
 	if currentRoom then
-		moduleScripts.Module_Events.flicker(currentRoom, 2)
+		moduleScripts.Module_Events.flicker(currentRoom, 4)
 	end
 
 	if G.LoadGithubModel then
